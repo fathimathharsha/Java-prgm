a058722d96854a728af6f39f3e0d23445c90d567
fathimathharsha <77012860+fathimathharsha@users.noreply.github.com>
2021-09-06 10:20:17 -0400

    Add files via upload

---
+++ CO3-Prgms/CO3Pgrm6.java
@@ -0,0 +1,24 @@
+class StringFormatter 
+{  
+public static String reverseWord(String str)
+{  
+    String words[]=str.split("\\s");  
+    String reverseWord="";  
+    for(String w:words)
+	{  
+        StringBuilder sb=new StringBuilder(w);  
+        sb.reverse();  
+        reverseWord+=sb.toString()+" ";  
+    }  
+    return reverseWord.trim();  
+}  
+} 
+
+public class CO3Pgrm6 
+{  
+public static void main(String[] args) 
+{  
+    System.out.println(StringFormatter.reverseWord("Hai World"));  
+    System.out.println(StringFormatter.reverseWord("I'm Fathimath Dilsha"));    
+    }  
+}  
